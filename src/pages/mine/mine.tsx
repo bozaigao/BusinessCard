2bc1b130cbae300fdf5480d86c9482c4d8406464
波仔糕 <1054539528@qq.com>
2019-12-22 09:40:41 +0800

    重新修改我的界面

--- src/pages/mine/mine.tsx
+++ src/pages/mine/mine.tsx
@@ -9,13 +9,19 @@ import Taro, {Component, Config} from '@tarojs/taro'
 import {Image, ScrollView, Text, View} from '@tarojs/components'
 import CustomSafeAreaView from "../../compoments/safe-area-view";
 import {
+  absB,
+  absR,
+  bdColor,
   bgColor,
+  bo,
   color,
   commonStyles,
   default as styles,
   fSize,
   h,
+  iphoneX,
   ml,
+  mr,
   mt,
   pl,
   pr,
@@ -24,15 +30,14 @@ import {
   wRatio
 } from "../../utils/style";
 import {styleAssign} from "../../utils/datatool";
-import TopHeader from "../../compoments/top-header";
+import TouchableButton from "../../compoments/touchable-button";
 
 
 interface Props {
 }
 
 interface State {
-  centerBarList: any;
-  dataList: any;
+  marginTop: any;
 }
 
 class Mine extends Component<Props, State> {
@@ -48,13 +53,24 @@ class Mine extends Component<Props, State> {
     disableScroll: true
   }
 
-  state = {
-    centerBarList: [],
-    dataList: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      marginTop: 0,
+    }
   }
 
   componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps)
+  }
+
+  componentWillMount() {
+    //这里只要是针对微信小程序设置自定义tabBar后的iphoneX高度适配
+    if (iphoneX()) {
+      this.setState({marginTop: 43});
+    } else {
+      this.setState({marginTop: 15});
+    }
   }
 
   componentWillUnmount() {
@@ -69,81 +85,114 @@ class Mine extends Component<Props, State> {
 
   render() {
 
+    let {marginTop} = this.state;
+
     return (
-      <CustomSafeAreaView customStyle={styleAssign([bgColor(commonStyles.whiteColor)])}
-                          notNeedBottomPadding={true}>
-        <TopHeader title={'我的'}/>
+      <CustomSafeAreaView customStyle={styleAssign([bgColor(commonStyles.pageDefaultBackgroundColor)])}
+                          notNeedBottomPadding={true} notNeedTopPadding={true}>
         <ScrollView
-          style={styleAssign([styles.uf1, bgColor(commonStyles.pageDefaultBackgroundColor)])}
+          style={styleAssign([wRatio(100)])}
           scrollY>
-          {/*我的简略信息*/}
-          <View style={styleAssign([wRatio(100), h(106), bgColor(commonStyles.whiteColor), styles.udr, styles.uac])}>
-            <Image style={styleAssign([w(66), h(66), radiusA(33), ml(20)])}
-                   src={require('../../assets/ico_default.jpeg')}/>
-            <View style={styleAssign([ml(6)])}>
-              <View style={styleAssign([styles.uac, styles.udr])}>
-                <View style={styleAssign([styles.uac, styles.udr, styles.uae])}>
-                  <Text style={styleAssign([fSize(18), color(commonStyles.colorTheme)])}>王嘉怡</Text>
-                  <Text
-                    style={styleAssign([fSize(12), color('#A9A9A9'), ml(8)])}>销售经理</Text>
-                </View>
-                <View style={styleAssign([styles.uac, styles.udr, ml(80)])}>
-                  <Text style={styleAssign([fSize(12), color('#A9A9A9')])}>完善名片</Text>
-                  <Image style={styleAssign([w(6), h(12), ml(8)])}
-                         src={require('../../assets/ico_next.png')}/>
-                </View>
-              </View>
-              <Text style={styleAssign([fSize(10), color('#A9A9A9'), mt(6)])}>美克美家家居集团股份有限公司</Text>
-              {/*进度展示*/}
-              <View style={styleAssign([styles.uac, styles.udr, mt(16)])}>
-                <View style={styleAssign([bgColor('#E8E8E8'), w(140), h(6), radiusA(3)])}>
-                  <View style={styleAssign([bgColor('#E8E8E8'), wRatio(80), h(6), bgColor('#0F56C5'), radiusA(3)])}/>
-                </View>
-                <Text style={styleAssign([fSize(10), color('#A9A9A9'), ml(8)])}>完善度96%</Text>
+          <View style={styleAssign([wRatio(100)])}>
+            <View style={styleAssign([wRatio(100), h(182), bgColor(commonStyles.colorTheme)])}>
+              {/*我的*/}
+              <View
+                style={styleAssign([mt(marginTop), wRatio(100), h(44), styles.ujb, styles.udr, styles.uac])}>
+                <Image style={styleAssign([w(22), h(14), ml(20)])} src={require('../../assets/ico_switch_white.png')}/>
+                <Text style={styleAssign([fSize(19), color(commonStyles.whiteColor)])}>我的</Text>
+                <View style={styleAssign([w(22), h(22), bgColor(commonStyles.transparent), mr(20)])}/>
               </View>
             </View>
+            <View style={styleAssign([wRatio(100), h(220), bgColor(commonStyles.whiteColor)])}/>
+            {/*个人信息展示*/}
+            <View style={styleAssign([wRatio(100), styles.uac, styles.upa, absB(10)])}>
+              <View style={styleAssign([w(120), h(120)])}>
+                <Image
+                  style={styleAssign([w(120), h(120), radiusA(60), bo(3), bdColor(commonStyles.whiteColor), {borderStyle: 'solid'}])}
+                  src={require('../../assets/ico_default.jpeg')}/>
+                <Image
+                  style={styleAssign([w(23), h(23), radiusA(11.5), styles.upa, absB(2), absR(2)])}
+                  src={require('../../assets/ico_nv.png')}/>
+              </View>
+              <Text style={styleAssign([fSize(20), color('#343434'), mt(15)])}>王嘉怡</Text>
+              <Text style={styleAssign([fSize(16), color('#727272'), mt(4)])}>美克美家家居股份有限公司</Text>
+              <Text style={styleAssign([fSize(14), color('#727272'), mt(4)])}>四川美术学院</Text>
+              <Text style={styleAssign([fSize(14), color('#727272'), mt(4)])}>四川 成都</Text>
+              <Text style={styleAssign([fSize(14), color('#727272'), mt(4)])}>耐用消耗品</Text>
+            </View>
+            {/*设置*/}
+            <View style={styleAssign([styles.uac, styles.upa, absR(10), absB(180)])}>
+              <Image style={styleAssign([w(19), h(19)])} src={require('../../assets/ico_setting.png')}/>
+              <Image style={styleAssign([w(21), h(19), mt(70)])} src={require('../../assets/ico_edit.png')}/>
+            </View>
           </View>
+          {/*名片竞争力*/}
+          <View
+            style={styleAssign([wRatio(100), h(101), mt(10), styles.ujb, styles.udr, bgColor(commonStyles.whiteColor)])}>
+            <View style={styleAssign([styles.uac, styles.udr, h(20), ml(20), mt(17)])}>
+              <Text style={styleAssign([fSize(16), color('#343434')])}>名片竞争力：</Text>
+              <Text style={styleAssign([fSize(16), color('#343434')])}>中级</Text>
+            </View>
+            <Image style={styleAssign([w(12), h(5), mr(20), mt(20)])} src={require('../../assets/ico_down2.png')}/>
+          </View>
+          {/*个人简介*/}
+          <View
+            style={styleAssign([wRatio(100), h(101), mt(10), styles.ujb, styles.udr, bgColor(commonStyles.whiteColor)])}>
+            <View style={styleAssign([ml(20), mt(17)])}>
+              <Text style={styleAssign([fSize(16), color('#343434')])}>个人简介</Text>
+              <Text style={styleAssign([fSize(14), color('#979797')])}>完善个人简介，拥有精美名片</Text>
+            </View>
+            <Image style={styleAssign([w(12), h(5), mr(20), mt(30)])} src={require('../../assets/ico_down2.png')}/>
+          </View>
+          {/*我的照片和视频*/}
+          {
+            [{
+              title: '我的照片',
+              subtitle1: '添加照片',
+              subtitle2: '让客户更全面了解你',
+              ico: require('../../assets/ico_camera.png')
+            },
+              {
+                title: '我的视频',
+                subtitle1: '添加视频',
+                subtitle2: '让客户更全面了解你',
+                ico: require('../../assets/ico_play.png')
+              }].map((value, index) => {
+              return (<View key={index}
+                            style={styleAssign([wRatio(100), h(264), mt(10), styles.uac, bgColor(commonStyles.whiteColor)])}>
+                <View style={styleAssign([wRatio(100)])}>
+                  <Text style={styleAssign([fSize(16), color('#0C0C0C'), ml(20), mt(17)])}>{value.title}</Text>
+                </View>
+                <View style={styleAssign([w(335), h(1), mt(12), bgColor(commonStyles.pageDefaultBackgroundColor)])}/>
+                <View
+                  style={styleAssign([w(335), h(176), mt(16), radiusA(4), styles.uac, styles.ujc, bgColor(commonStyles.pageDefaultBackgroundColor)])}>
+                  <View style={styleAssign([styles.uac])}>
+                    <TouchableButton
+                      customStyle={styleAssign([w(40), h(40), radiusA(20), bgColor(commonStyles.whiteColor),
+                        styles.uac, styles.ujc])}>
+                      <Image style={styleAssign([w(21), h(19)])} src={require('../../assets/ico_camera.png')}/>
+                    </TouchableButton>
+                    <Text style={styleAssign([fSize(12), color('#ACADAD'), mt(10)])}>{value.subtitle1}</Text>
+                    <Text style={styleAssign([fSize(12), color('#ACADAD'), mt(4)])}>{value.subtitle2}</Text>
+                  </View>
+                </View>
+              </View>);
+            })
+          }
           {/*企业信息*/}
           <View
-            style={styleAssign([wRatio(100), h(60), mt(10), pl(20), pr(20), bgColor(commonStyles.whiteColor), styles.udr, styles.uac, styles.ujb])}>
-            <Text style={styleAssign([fSize(14), color(commonStyles.colorTheme)])}>企业信息</Text>
-            <View style={styleAssign([styles.uac, styles.udr])}>
-              <Text style={styleAssign([fSize(12), color('#A9A9A9')])}>编辑</Text>
-              <Image style={styleAssign([w(6), h(12), ml(6)])} src={require('../../assets/ico_next.png')}/>
+            style={styleAssign([wRatio(100), h(80), mt(10), bgColor(commonStyles.whiteColor), styles.uac, styles.ujc,
+              pl(20), pr(20)])}>
+            <View style={styleAssign([wRatio(100), styles.uac, styles.ujb, styles.udr])}>
+              <View>
+                <Text style={styleAssign([fSize(16), color('#0C0C0C'), mt(4)])}>企业信息</Text>
+                <Text style={styleAssign([fSize(14), color('#979797'), mt(4)])}>完善企业信息，提升你的信赖度</Text>
+              </View>
+              <TouchableButton customStyle={styleAssign([styles.uac, styles.udr])}>
+                <Text style={styleAssign([fSize(12), color('#A9A9A9')])}>编辑</Text>
+                <Image style={styleAssign([w(7), h(12), ml(6)])} src={require('../../assets/ico_next.png')}/>
+              </TouchableButton>
             </View>
-          </View>
-          <View style={styleAssign([wRatio(100), mt(10), styles.uac, bgColor(commonStyles.whiteColor)])}>
-            {
-              ['商城', '海报', '工具箱', '名片夹', '任务中心'].map((value, index) => {
-                return (<View style={styleAssign([wRatio(100), styles.uac])}  key={index}><View
-                  style={styleAssign([wRatio(100), h(60), pl(20), pr(20), bgColor(commonStyles.whiteColor), styles.udr, styles.uac, styles.ujb])}>
-                  <Text style={styleAssign([fSize(14), color(commonStyles.colorTheme)])}>{value}</Text>
-                  <Image style={styleAssign([w(6), h(12), ml(6)])} src={require('../../assets/ico_next.png')}/>
-                </View>
-                  {
-                    index !== 4 &&
-                    <View style={styleAssign([wRatio(90), h(1), bgColor(commonStyles.pageDefaultBackgroundColor)])}/>
-                  }
-                </View>);
-              })
-            }
-          </View>
-          <View style={styleAssign([wRatio(100), mt(10), styles.uac, bgColor(commonStyles.whiteColor)])}>
-            {
-              ['使用小技巧', '投诉与建议', '关于极致名片'].map((value, index) => {
-                return (<View style={styleAssign([wRatio(100), styles.uac])}><View
-                  key={index}
-                  style={styleAssign([wRatio(100), h(60), pl(20), pr(20), bgColor(commonStyles.whiteColor), styles.udr, styles.uac, styles.ujb])}>
-                  <Text style={styleAssign([fSize(14), color(commonStyles.colorTheme)])}>{value}</Text>
-                  <Image style={styleAssign([w(6), h(12), ml(6)])} src={require('../../assets/ico_next.png')}/>
-                </View>
-                  {
-                    index !== 2 &&
-                    <View style={styleAssign([wRatio(90), h(1), bgColor(commonStyles.pageDefaultBackgroundColor)])}/>
-                  }
-                </View>);
-              })
-            }
           </View>
         </ScrollView>
       </CustomSafeAreaView>
