17ff103350cd12373e78db2e37fed73b25fefb4e
波仔糕 <1054539528@qq.com>
2020-02-09 13:26:08 +0800

    fix

--- src/pages/businesscard/my_collect.tsx
+++ src/pages/businesscard/my_collect.tsx
@@ -34,7 +34,7 @@ import {
 import {connect} from "@tarojs/redux";
 import * as actions from '../../actions/business_card';
 import {CollectItemModel, User} from "../../const/global";
-import {ScrollView, Text, View} from "@tarojs/components";
+import {Image, ScrollView, Text, View} from "@tarojs/components";
 import CollectItem from "./collect-item";
 import BusinessCardRemoveNoticeModal from "./businesscard-remove-notice";
 
@@ -137,27 +137,35 @@ class MyCollect extends Component<Props, State> {
       }} customStyle={styleAssign([bgColor(commonStyles.whiteColor)])}>
         <View style={styleAssign([styles.uf1, bgColor(commonStyles.pageDefaultBackgroundColor)])}>
           <View
-            style={styleAssign([wRatio(100), h(44), styles.udr, styles.uac, styles.ujc, bgColor(commonStyles.whiteColor)])}>
-            <View style={styleAssign([styles.uac, styles.udr])}
-                  onClick={() => {
-                    this.setState({currentIndex: 0});
-                  }}>
-              <View style={styleAssign([styles.uac])}>
-                <Text style={styleAssign([fSize(18), color(currentIndex === 0 ? '#E2BB7B' : '#0C0C0C')])}>访客</Text>
-                <View
-                  style={styleAssign([w(36), h(2), bgColor(currentIndex === 0 ? '#E2BB7B' : commonStyles.whiteColor), mt(10)])}/>
+            style={styleAssign([wRatio(100), h(44), styles.udr, styles.uac, styles.ujb, bgColor(commonStyles.whiteColor)])}>
+            <Image style={styleAssign([w(22), h(22), ml(20)])}
+                   src={require('../../assets/ico_back.png')}
+                   onClick={() => {
+                     Taro.navigateBack();
+                   }}/>
+            <View style={styleAssign([styles.uac, styles.udr])}>
+              <View style={styleAssign([styles.uac, styles.udr])}
+                    onClick={() => {
+                      this.setState({currentIndex: 0});
+                    }}>
+                <View style={styleAssign([styles.uac])}>
+                  <Text style={styleAssign([fSize(18), color(currentIndex === 0 ? '#E2BB7B' : '#0C0C0C')])}>访客</Text>
+                  <View
+                    style={styleAssign([w(36), h(2), bgColor(currentIndex === 0 ? '#E2BB7B' : commonStyles.whiteColor), mt(10)])}/>
+                </View>
+              </View>
+              <View style={styleAssign([styles.uac, styles.udr, ml(24)])}
+                    onClick={() => {
+                      this.setState({currentIndex: 1});
+                    }}>
+                <View style={styleAssign([styles.uac])}>
+                  <Text style={styleAssign([fSize(18), color(currentIndex === 1 ? '#E2BB7B' : '#0C0C0C')])}>收藏</Text>
+                  <View
+                    style={styleAssign([w(36), h(2), bgColor(currentIndex === 1 ? '#E2BB7B' : commonStyles.whiteColor), mt(10)])}/>
+                </View>
               </View>
             </View>
-            <View style={styleAssign([styles.uac, styles.udr, ml(24)])}
-                  onClick={() => {
-                    this.setState({currentIndex: 1});
-                  }}>
-              <View style={styleAssign([styles.uac])}>
-                <Text style={styleAssign([fSize(18), color(currentIndex === 1 ? '#E2BB7B' : '#0C0C0C')])}>收藏</Text>
-                <View
-                  style={styleAssign([w(36), h(2), bgColor(currentIndex === 1 ? '#E2BB7B' : commonStyles.whiteColor), mt(10)])}/>
-              </View>
-            </View>
+            <View/>
           </View>
           {
             currentIndex === 1 &&
