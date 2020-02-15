6924011f2c3c0ff3bceeb828a6b4b7a2eca1338a
波仔糕 <1054539528@qq.com>
2020-02-15 18:13:06 +0800

    fix

--- src/compoments/top-header/index.tsx
+++ src/compoments/top-header/index.tsx
@@ -9,7 +9,6 @@ import Taro, {Component} from "@tarojs/taro";
 import {Image, Text, View} from "@tarojs/components";
 import {styleAssign} from "../../utils/datatool";
 import styles, {bgColor, color, commonStyles, fSize, h, ml, mr, w, wRatio} from "../../utils/style";
-import {cloudBaseUrl} from "../../api/httpurl";
 
 interface Props {
   title: string;
@@ -37,7 +36,7 @@ export default class TopHeader extends Component<Props, State> {
       <View
         style={styleAssign([wRatio(100), h(44), styles.ujb, styles.udr, styles.uac, bgColor(backgroundColor ? backgroundColor : commonStyles.whiteColor)])}>
         <Image style={styleAssign([w(22), h(22), ml(20)])}
-               src={textColor ? `${cloudBaseUrl}ico_back_white.png` : `${cloudBaseUrl}ico_back.png`}
+               src={textColor ? require('../../assets/ico_back_white.png') : require('../../assets/ico_back.png')}
                onClick={() => {
                  if (customCallback) {
                    customCallback();
