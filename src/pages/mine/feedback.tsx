854a62dafdc93794205e54aca43f8f26fda3ad4e
波仔糕 <1054539528@qq.com>
2020-02-22 15:23:06 +0800

    fix

--- src/pages/mine/feedback.tsx
+++ src/pages/mine/feedback.tsx
@@ -10,18 +10,18 @@ import Taro, {Component, Config} from '@tarojs/taro'
 import CustomSafeAreaView from "../../compoments/safe-area-view";
 //@ts-ignore
 import {styleAssign} from "../../utils/datatool";
-import {bgColor, commonStyles, default as styles, h, mt, pa, wRatio} from "../../utils/style";
+import {absB, absR, bgColor, color, commonStyles, default as styles, fSize, h, mt, pa, wRatio} from "../../utils/style";
 import {connect} from "@tarojs/redux";
 import * as actions from '../../actions/login';
 import TopHeader from "../../compoments/top-header";
-import {Textarea, View} from "@tarojs/components";
+import {Text, Textarea, View} from "@tarojs/components";
 import BottomButon from "../../compoments/bottom-buton";
 
 interface Props {
 }
 
 interface State {
-
+  content: string;
 }
 
 @connect(state => state.login, {...actions})
@@ -44,10 +44,14 @@ class Feedback extends Component<Props, State> {
   constructor(props) {
     super(props);
     console.log(this.viewRef);
+    this.state = {
+      content: ''
+    }
   }
 
 
   render() {
+    let {content} = this.state;
 
     return (
       <CustomSafeAreaView ref={(ref) => {
@@ -55,10 +59,20 @@ class Feedback extends Component<Props, State> {
       }} customStyle={styleAssign([bgColor(commonStyles.whiteColor)])}>
         <TopHeader title={'投诉与建议'}/>
         <View style={styleAssign([styles.uf1, bgColor(commonStyles.whiteColor)])}>
-         <Textarea
-           style={styleAssign([wRatio(80), {marginLeft: '5%'}, h(160), pa(20), bgColor(commonStyles.pageDefaultBackgroundColor),
-             mt(20)])}
-           value={''} placeholder={'请输入你的反馈意见'}/>
+          <View style={styleAssign([wRatio(100), h(160)])}>
+            <Textarea
+              maxlength={600}
+              style={styleAssign([wRatio(80), {marginLeft: '5%'}, h(160), pa(20), bgColor(commonStyles.pageDefaultBackgroundColor),
+                mt(20)])}
+              value={''} placeholder={'请输入您的意见或建议，提交以后我们的工作人员会及时查看的~'}
+              onInput={(e) => {
+                this.setState({content: e.detail.value});
+              }}/>
+            <View style={styleAssign([styles.uac, styles.udr, styles.upa, absR(30), absB(10)])}>
+              <Text style={styleAssign([fSize(12), color('#979797')])}>{content.length}</Text>
+              <Text style={styleAssign([fSize(12), color('#CECECE')])}>/600</Text>
+            </View>
+          </View>
         </View>
         {/*提交*/}
         <BottomButon title={'提交'} onClick={() => {
