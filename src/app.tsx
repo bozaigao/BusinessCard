9fa62711c6004515bbd0b72ddb254a720111f7f4
波仔糕 <1054539528@qq.com>
2020-03-01 15:41:04 +0800

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -63,6 +63,7 @@ class App extends Component {
    */
   config: Config = {
     pages: [
+      "pages/help/how_share_card",
       "pages/help/how_perform_card",
       "pages/help/introduce",
       //主界面
