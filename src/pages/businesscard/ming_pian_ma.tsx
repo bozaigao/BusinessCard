d71a7acb9e1531e78b15bb8384a3866974e9b477
波仔糕 <1054539528@qq.com>
2020-03-08 11:42:47 +0800

    fix

--- src/pages/businesscard/ming_pian_ma.tsx
+++ src/pages/businesscard/ming_pian_ma.tsx
@@ -21,7 +21,7 @@ import styles, {
   ml,
   mt,
   pl,
-  pr,
+  pr, radiusA,
   w,
   wRatio
 } from "../../utils/style";
@@ -29,9 +29,11 @@ import {connect} from "@tarojs/redux";
 import * as actions from '../../actions/login';
 import TopHeader from "../../compoments/top-header/index";
 import {cloudBaseUrl} from "../../api/httpurl";
+import {User} from "../../const/global";
 
 interface Props {
   suggestionAdd: any;
+  userInfo: User;
 }
 
 interface State {
@@ -64,6 +66,7 @@ class MingPianMa extends Component<Props, State> {
 
 
   render() {
+    let {userInfo} = this.props;
 
     return (
       <CustomSafeAreaView ref={(ref) => {
@@ -75,32 +78,35 @@ class MingPianMa extends Component<Props, State> {
             <View style={styleAssign([w(335), h(189)])}>
               <Image style={styleAssign([w(335), h(189), styles.upa, absT(0)])}
                      src={require('../../assets/ico_mingpianma_bg.png')}/>
-              <View style={styleAssign([wRatio(100), styles.upa, pl(17),pr(17), absT(17)])}>
+              <View style={styleAssign([wRatio(100), styles.upa, pl(17), pr(17), absT(17)])}>
                 <View style={styleAssign([wRatio(100), styles.udr, styles.uac, styles.ujb])}>
                   <View>
-                    <Text style={styleAssign([fSize(18), fWeight('bold'), color(commonStyles.whiteColor)])}>王嘉怡</Text>
-                    <Text style={styleAssign([fSize(12), color(commonStyles.whiteColor)])}>富森美家居(成都分部)·销售经理</Text>
+                    <Text
+                      style={styleAssign([fSize(18), fWeight('bold'), color(commonStyles.whiteColor)])}>{userInfo.name}</Text>
+                    <Text
+                      style={styleAssign([fSize(12), color(commonStyles.whiteColor)])}>{`${userInfo.company}·${userInfo.position}`}</Text>
                   </View>
-                  <Image style={styleAssign([w(50), h(50)])} src={`${cloudBaseUrl}ico_default.png`}/>
+                  <Image style={styleAssign([w(50), h(50), radiusA(25)])}
+                         src={userInfo.avatar ? userInfo.avatar : `${cloudBaseUrl}ico_default.png`}/>
                 </View>
                 {/*电话号码*/}
                 <View style={styleAssign([styles.uac, styles.udr, mt(28)])}>
                   <Image style={styleAssign([w(12), h(10), ml(8)])} src={`${cloudBaseUrl}ico_card_mobile.png`}/>
                   <Text
-                    style={styleAssign([fSize(12), color('#343434'), color(commonStyles.whiteColor), ml(6)])}>17311239269</Text>
+                    style={styleAssign([fSize(12), color('#343434'), color(commonStyles.whiteColor), ml(6)])}>{userInfo.phone}</Text>
                 </View>
                 {/*邮箱*/}
                 <View style={styleAssign([styles.uac, styles.udr, mt(8)])}>
                   <Image style={styleAssign([w(12), h(10), ml(8)])} src={`${cloudBaseUrl}ico_card_email.png`}/>
                   <Text
-                    style={styleAssign([fSize(12), color('#343434'), color(commonStyles.whiteColor), ml(6)])}>邮箱信息未对外公开</Text>
+                    style={styleAssign([fSize(12), color('#343434'), color(commonStyles.whiteColor), ml(6)])}>{userInfo.email ? userInfo.email : '邮箱信息未对外公开'}</Text>
                 </View>
                 {/*地址*/}
                 <View style={styleAssign([styles.udr, mt(8)])}>
                   <Image style={styleAssign([w(9), h(11), ml(8), mt(4)])}
                          src={`${cloudBaseUrl}ico_card_location.png`}/>
                   <Text
-                    style={styleAssign([fSize(12), color('#343434'), color(commonStyles.whiteColor), ml(6)])}>{`四川省成都市武侯区盛和\n二路18号富森美家居`}</Text>
+                    style={styleAssign([fSize(12), color('#343434'), color(commonStyles.whiteColor), ml(6)])}>{userInfo.detailAddress}</Text>
                 </View>
               </View>
             </View>
