042eed149dba94d5f81e7f2f15d2ad93d8ef03b7
波仔糕 <1054539528@qq.com>
2020-03-13 01:13:48 +0800

    fix

--- src/app.tsx
+++ src/app.tsx
@@ -63,7 +63,6 @@ class App extends Component {
    */
   config: Config = {
     pages: [
-      "pages/test",
       //主界面
       "pages/businesscard",
       "pages/radarscan",
--- src/pages/businesscard/mingpian_haibao.tsx
+++ src/pages/businesscard/mingpian_haibao.tsx
@@ -1,40 +1,13 @@
-/**
- * @filename mingpian_haibao.tsx
- * @author 何晏波
- * @QQ 1054539528
- * @date 2019/12/17
- * @Description: 名片海报
- */
 import Taro, {Component, Config} from '@tarojs/taro'
-//@ts-ignore
-import CustomSafeAreaView from "../../compoments/safe-area-view/index";
-//@ts-ignore
+import {View} from '@tarojs/components'
 import {styleAssign} from "../../utils/datatool";
-import {
-  absB,
-  absL,
-  absR,
-  absT,
-  bgColor,
-  color,
-  commonStyles,
-  default as styles,
-  fSize,
-  fWeight,
-  h,
-  ml,
-  mt,
-  radiusA,
-  w,
-  wRatio
-} from "../../utils/style";
-import {connect} from "@tarojs/redux";
-import * as actions from '../../actions/login';
+import {bgColor, commonStyles, default as styles} from "../../utils/style";
 import TopHeader from "../../compoments/top-header/index";
-import {Image, Text, View} from "@tarojs/components";
-import {cloudBaseUrl} from "../../api/httpurl";
-import BottomButon from "../../compoments/bottom-buton";
+import * as actions from '../../actions/login';
+import CustomSafeAreaView from "../../compoments/safe-area-view/index";
+import BottomButon from "../../compoments/bottom-buton/index";
 import {User} from "../../const/global";
+import {connect} from "@tarojs/redux";
 
 interface Props {
   userInfo: User;
@@ -46,98 +19,243 @@ interface State {
 
 @connect(state => state.login, {...actions})
 class MingpianHaibao extends Component<Props, State> {
-
   private viewRef;
-
-
-  /**
-   * 指定config的类型声明为: Taro.Config
-   *
-   * 由于 typescript 对于 object 类型推导只能推出 Key 的基本类型
-   * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
-   * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
-   */
   config: Config = {
-    disableScroll: true
+    navigationBarTitleText: 'canvas测试'
   }
 
   constructor(props) {
-    super(props);
-    console.log(this.viewRef);
+    super(props)
+    this.state = {
+      imageTempPath: ''
+    }
+  }
+
+  componentWillMount(): void {
+    this.drawBall()
+  }
+
+  drawBall() {
+    this.viewRef&&this.viewRef.showLoading();
+    let {userInfo} = this.props;
+
+    const context = Taro.createCanvasContext('canvas', this)
+    //@ts-ignore
+    const that = this;
+
+    Taro.getImageInfo({
+      src: 'https://6275-business-card-8687h-1301418170.tcb.qcloud.la/assets/ico_business_card_bg.png?sign=7953c9294cdf7a68d4dd508bb6d5f72b&t=1583943566',
+    }).then((res) => {
+      this.roundRectColor(context, 0, 0, 335, 434, 16);
+      //@ts-ignore
+      context.drawImage(res.path, 10, 10, 313, 194);
+      //电话
+      Taro.getImageInfo({
+        src: `https://6275-business-card-8687h-1301418170.tcb.qcloud.la/assets/ico_card_mobile.png?sign=16ccd18da0a7c3bbcbb3bf1f8a582b0d&t=1584026985`,
+      }).then((res) => {
+        //@ts-ignore
+        context.drawImage(res.path, 295, 90, 11, 9);
+        context.setFontSize(12);
+        context.setFillStyle('#343434');
+        context.setTextAlign('right');
+        context.fillText(userInfo.phone, 290, 100);
+        //微信
+        Taro.getImageInfo({
+          src: `https://6275-business-card-8687h-1301418170.tcb.qcloud.la/assets/ico_card_wechat.png?sign=d69e311e5b1e06c521c064611bd9d30a&t=1584028318`,
+        }).then((res) => {
+          //@ts-ignore
+          context.drawImage(res.path, 295, 110, 12, 10);
+          context.setTextAlign('right');
+          context.fillText(userInfo.wechat, 290, 120);
+          //邮箱
+          Taro.getImageInfo({
+            src: `https://6275-business-card-8687h-1301418170.tcb.qcloud.la/assets/ico_card_email.png?sign=f35e0d13f139b041fdb849c2e143e5ce&t=1584030793`,
+          }).then((res) => {
+            //@ts-ignore
+            context.drawImage(res.path, 295, 130, 12, 10);
+            context.setTextAlign('right');
+            context.fillText(userInfo.email ? userInfo.email : '邮箱信息未对外公开', 290, 140);
+            //地址
+            Taro.getImageInfo({
+              src: `https://6275-business-card-8687h-1301418170.tcb.qcloud.la/assets/ico_card_location.png?sign=c3abda7fa28594034f71a597086f5864&t=1584030919`,
+            }).then((res) => {
+              //@ts-ignore
+              context.drawImage(res.path, 295, 150, 12, 10);
+              context.setTextAlign('right');
+              context.fillText(userInfo.detailAddress, 290, 160);
+              //小程序码
+              Taro.getImageInfo({
+                src: `https://cardapplication.oss-cn-chengdu.aliyuncs.com/picture/1a6fe6b5-a397-476c-8ddb-ab1c50d016fauser_20_1583967075205.jpg`,
+              }).then((res) => {
+                //@ts-ignore
+                context.drawImage(res.path, 15, 360, 44, 44);
+                context.setTextAlign('right');
+                Taro.getImageInfo({
+                  src: userInfo.avatar,
+                }).then((res) => {
+
+                  let arcWidth = 60;
+                  //x-轴坐标
+                  let xCoor = 30;
+                  //y-轴坐标
+                  let yCoor = 20;
+
+                  context.beginPath();
+                  context.arc(xCoor + arcWidth / 2, yCoor + arcWidth / 2, arcWidth / 2, 0, Math.PI * 2, false)
+                  context.clip();
+                  //@ts-ignore
+                  context.drawImage(res.path, xCoor, yCoor, arcWidth, arcWidth);
+                  context.restore();
+                  context.setFontSize(18);
+                  context.setFillStyle(commonStyles.colorTheme);
+                  context.fillText(userInfo.name, 34, 115);
+                  context.setFontSize(12);
+                  context.fillText(userInfo.position, 100, 115);
+                  context.fillText(userInfo.company, 34, 140);
+                  context.setFontSize(14);
+                  context.fillText('您好,', 15, 240);
+                  that.fillTextWrap(context, `我是${userInfo.company}的 ${userInfo.position}${userInfo.name}`, 15, 260, 294, 20, 14);
+                  context.fillText('这是我的名片，请惠存。', 15, 300);
+                  context.fillText('谢谢!', 15, 320);
+                  context.setFillStyle('#E2BB7B');
+                  context.fillText('长按识别二维码 收下名片', 70, 390);
+                  context.setStrokeStyle(commonStyles.pageDefaultBackgroundColor);
+                  context.moveTo(0, 340);
+                  context.lineTo(335, 340);
+                  context.stroke();
+                  context.draw(false, () => {
+                    that.viewRef&&that.viewRef.hideLoading();
+                    Taro.canvasToTempFilePath({
+                      canvasId: 'canvas',
+                      success: function (res) {
+                        console.log('获得图片临时路径', res);
+                        // 获得图片临时路径
+                        that.setState({
+                          imageTempPath: res.tempFilePath
+                        })
+                      }
+                    })
+                  });
+                });
+              });
+            });
+          });
+        });
+      });
+    });
+  }
+
+  /**
+   * @author 何晏波
+   * @QQ 1054539528
+   * @date 2020/3/12
+   * @function: 绘制圆角填充色矩形
+   */
+  roundRectColor(context, x, y, w, h, r) {  //绘制圆角矩形（纯色填充）
+    context.save();
+    context.setFillStyle(commonStyles.whiteColor);
+    context.setStrokeStyle(commonStyles.whiteColor)
+    context.setLineJoin('round');  //交点设置成圆角
+    context.setLineWidth(r);
+    context.strokeRect(x + r / 2, y + r / 2, w - r, h - r);
+    context.fillRect(x + r, y + r, w - r * 2, h - r * 2);
+    context.stroke();
+    context.closePath();
   }
 
 
-  render() {
-    let {userInfo} = this.props;
+  /**
+   * @author 何晏波
+   * @QQ 1054539528
+   * @date 2020/3/12
+   * @function: 保存s存图片
+   */
+  saveImage() {
+    // 查看是否授权
+    Taro.getSetting({
+      complete() {
+        console.log(444)
+      }
+    }).then(res => {
+      if (res.authSetting['scope.writePhotosAlbum']) {
+        console.log('保存图片', this.state.imageTempPath);
+        Taro.saveImageToPhotosAlbum({
+          filePath: this.state.imageTempPath
+        }).then(res => {
+          console.log(res)
+        })
+      } else {
+        Taro.authorize({
+          scope: 'scope.writePhotosAlbum',
+        }).then(() => {
+          Taro.saveImageToPhotosAlbum({
+            filePath: this.state.imageTempPath
+          }).then(res => {
+            console.log(res)
+          })
+        })
+      }
+    }).catch((e) => {
+      console.log(e)
+    })
+  }
 
+  /**
+   * @author 何晏波
+   * @QQ 1054539528
+   * @date 2020/3/12
+   * @function: 文字换行
+   */
+  fillTextWrap(ctx, text, x, y, maxWidth, lineHeight, fontSize) {
+    // 设定默认最大宽度
+    const systemInfo = Taro.getSystemInfoSync();
+    const deciveWidth = systemInfo.screenWidth;
+
+    maxWidth = maxWidth || deciveWidth;
+    lineHeight = lineHeight || 20;
+    // 校验参数
+    if (typeof text !== 'string' || typeof x !== 'number' || typeof y !== 'number') {
+      return;
+    }
+    // 字符串分割为数组
+    const arrText = text.split('');
+    // 当前字符串及宽度
+    let currentText = '';
+    let currentWidth;
+    ctx.setFontSize(fontSize);
+    ctx.setFillStyle('#3A3A3A');
+    ctx.setTextAlign('justify');
+    for (let letter of arrText) {
+      currentText += letter;
+      currentWidth = ctx.measureText(currentText).width;
+      if (currentWidth > maxWidth) {
+        ctx.fillText(currentText, x, y);
+        currentText = '';
+        y += lineHeight;
+      }
+    }
+    if (currentText) {
+      ctx.fillText(currentText, x, y);
+    }
+  }
+
+  render() {
     return (
       <CustomSafeAreaView ref={(ref) => {
         this.viewRef = ref;
       }} customStyle={styleAssign([bgColor(commonStyles.whiteColor)])}>
         <TopHeader title={'名片海报'}/>
-        <View style={styleAssign([styles.uf1, bgColor(commonStyles.pageDefaultBackgroundColor), styles.uac])}>
-          <View
-            style={styleAssign([wRatio(90), h(434), bgColor(commonStyles.whiteColor), mt(20), radiusA(4), styles.uac])}>
-            {/*名片*/}
-            <View style={styleAssign([wRatio(100), h(204), bgColor('rgb(211,199,195)'), radiusA(10),
-              styles.udr, styles.uje])}>
-              <Image style={styleAssign([wRatio(100), h(204), styles.upa, absT(0)])}
-                     src={require('../../assets/ico_business_card_bg.png')}/>
-              <View style={styleAssign([wRatio(100), h(204), styles.upa, absT(0)])}>
-                <View
-                  style={styleAssign([wRatio(100), h(204), radiusA(10), styles.upa, absL(0), absT(0)])}/>
-                <View style={styleAssign([styles.upa, absL(20), absT(15)])}>
-                  <Image style={styleAssign([w(60), h(60), radiusA(30)])}
-                         src={userInfo.avatar}/>
-                  <View style={styleAssign([styles.uae, styles.udr, mt(6)])}>
-                    <Text style={styleAssign([fSize(18), fWeight('bold')])}>{userInfo.name}</Text>
-                    <Text style={styleAssign([fSize(12), ml(8)])}>{userInfo.position}</Text>
-                  </View>
-                  <Text style={styleAssign([fSize(12), color('#343434')])}>{userInfo.company}</Text>
-                </View>
-                <View style={styleAssign([styles.uae, styles.upa, absB(26), absR(24)])}>
-                  {/*电话号码*/}
-                  <View style={styleAssign([styles.uac, styles.udr])}>
-                    <Text
-                      style={styleAssign([fSize(12), color('#343434')])}>{userInfo.phone}</Text>
-                    <Image style={styleAssign([w(12), h(10), ml(8)])} src={`${cloudBaseUrl}ico_card_mobile.png`}/>
-                  </View>
-                  {/*微信号*/}
-                  <View style={styleAssign([styles.uac, styles.udr, mt(8)])}>
-                    <Text
-                      style={styleAssign([fSize(12), color('#343434')])}>{userInfo.wechat}</Text>
-                    <Image style={styleAssign([w(12), h(10), ml(8)])} src={`${cloudBaseUrl}ico_card_wechat.png`}/>
-                  </View>
-                  {/*邮箱*/}
-                  <View style={styleAssign([styles.uac, styles.udr, mt(8)])}>
-                    <Text
-                      style={styleAssign([fSize(12), color('#343434')])}>{userInfo.email ? userInfo.email : '邮箱信息未对外公开'}</Text>
-                    <Image style={styleAssign([w(12), h(10), ml(8)])} src={`${cloudBaseUrl}ico_card_email.png`}/>
-                  </View>
-                  {/*地址*/}
-                  <View style={styleAssign([styles.udr, mt(8)])}>
-                    <Text
-                      style={styleAssign([fSize(12), color('#343434')])}>{userInfo.detailAddress}</Text>
-                    <Image style={styleAssign([w(9), h(11), ml(8), mt(4)])} src={`${cloudBaseUrl}ico_card_location.png`}/>
-                  </View>
-                </View>
-              </View>
-            </View>
-            {/*简介*/}
-            <Text
-              style={styleAssign([w(294), mt(23), fSize(14), color('#343434')])}>{`您好，\n 我是${userInfo.company}的 ${userInfo.position}${userInfo.name}
-              这是我的名片，请惠存。\n谢谢！`}</Text>
-            <View style={styleAssign([mt(20), wRatio(100), h(1), bgColor(commonStyles.pageDefaultBackgroundColor)])}/>
-            <View style={styleAssign([wRatio(100), styles.uae, styles.udr])}>
-              <Image style={styleAssign([w(44), h(44), ml(16), mt(40)])}
-                     src={userInfo.wxacode}/>
-              <Text style={styleAssign([fSize(12), color('#E2BB7B'), ml(11)])}>长按识别二维码 收下名片</Text>
-            </View>
+        <View style={styleAssign([styles.uf1, bgColor(commonStyles.pageDefaultBackgroundColor)])}>
+          <View style={styleAssign([styles.uac])}>
+            <canvas style="width: 335px; height: 434px;background:#fff;margin-top:20px;border-radius:4px;"
+                    canvas-id="canvas"/>
+          </View>
+          <View style={styleAssign([styles.uf1, styles.uje])}>
+            <BottomButon title={'保存名片海报后分享'} onClick={() => {
+              this.saveImage();
+            }}/>
           </View>
         </View>
-        <BottomButon title={'保存名片海报后分享'} onClick={() => {
-
-        }}/>
       </CustomSafeAreaView>);
   }
 }
