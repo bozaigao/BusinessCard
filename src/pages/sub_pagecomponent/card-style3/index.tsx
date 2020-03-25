cff8a09cf46a031d3c0cdf3050c5940e65aef8a8
波仔糕 <1054539528@qq.com>
2020-03-25 13:35:19 +0800

    fix

--- src/pages/sub_pagecomponent/card-style3/index.tsx
+++ src/pages/sub_pagecomponent/card-style3/index.tsx
@@ -25,7 +25,6 @@ import styles, {
   w,
   wRatio
 } from "../../../utils/style";
-import {cloudBaseUrl} from "../../../api/httpurl";
 
 
 interface Props {
@@ -68,7 +67,7 @@ export default class CardStyle3 extends PureComponent<Props, State> {
               {
                 !hidePhone && <View style={styleAssign([styles.uac, styles.udr])}>
                   <Image style={styleAssign([w(11), h(9)])}
-                         src={`${cloudBaseUrl}ico_card_mobile_gray.png`}/>
+                         src={require('../../../assets/ico_card_mobile_gray.png')}/>
                   <Text
                     style={styleAssign([fSize(11), color('#343434'), ml(5)])}>15982468866</Text>
                 </View>
@@ -77,7 +76,7 @@ export default class CardStyle3 extends PureComponent<Props, State> {
               {
                 !hideWechat && <View style={styleAssign([styles.uac, styles.udr, mt(hidePhone ? 0 : 4)])}>
                   <Image style={styleAssign([w(12), h(10)])}
-                         src={`${cloudBaseUrl}ico_card_wechat_gray.png`}/>
+                         src={require('../../../assets/ico_card_wechat_gray.png')}/>
                   <Text
                     style={styleAssign([fSize(11), color('#343434'), ml(5)])}>98248866LSY</Text>
                 </View>
@@ -88,7 +87,7 @@ export default class CardStyle3 extends PureComponent<Props, State> {
               {
                 !hideEmail && <View style={styleAssign([styles.uac, styles.udr])}>
                   <Image style={styleAssign([w(12), h(10)])}
-                         src={`${cloudBaseUrl}ico_card_email_gray.png`}/>
+                         src={require('../../../assets/ico_card_email_gray.png')}/>
                   <Text
                     style={styleAssign([fSize(11), color('#343434'), ml(5)])}>982468866@168.com</Text>
                 </View>
@@ -97,7 +96,7 @@ export default class CardStyle3 extends PureComponent<Props, State> {
               {
                 !hideAddress && <View style={styleAssign([styles.udr, mt(hideEmail ? 0 : 4)])}>
                   <Image style={styleAssign([w(10), h(11), mt(4)])}
-                         src={`${cloudBaseUrl}ico_card_location_gray.png`}/>
+                         src={require('../../../assets/ico_card_location_gray.png')}/>
                   <Text
                     style={styleAssign([fSize(11), color('#343434'), ml(5)])}>地址信息未对外公开</Text>
                 </View>
