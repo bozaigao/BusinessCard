7f27caa77d3c7c3edded6ec5ddf531fd7bce726f
波仔糕 <1054539528@qq.com>
2020-04-11 14:59:47 +0800

    fix

--- src/pages/customer/LineChart.js
+++ src/pages/customer/LineChart.js
@@ -1,34 +1,51 @@
 import Taro, { Component } from "@tarojs/taro";
 import * as echarts from "./ec-canvas/echarts";
 
-function setChartData(chart, data) {
+function setChartData(chart) {
   let option = {
-    color: ['#3398DB'],
-    xAxis : [
-      {
-        type: 'category',
-        data: [],
-        axisTick: {
-          alignWithLabel: true
+    title: {
+      text: '测试下面legend的红色区域不应被裁剪',
+      left: 'center'
+    },
+    color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
+    legend: {
+      data: ['A', 'B', 'C'],
+      top: 50,
+      left: 'center',
+      backgroundColor: 'red',
+      z: 100
+    },
+    grid: {
+      containLabel: true
+    },
+    tooltip: {
+      show: true,
+      trigger: 'axis'
+    },
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+      // show: false
+    },
+    yAxis: {
+      x: 'center',
+      type: 'value',
+      splitLine: {
+        lineStyle: {
+          type: 'dashed'
         }
       }
-    ],
-    yAxis : [
-      {
-        type : 'value'
-      }
-    ],
-    series : []
+      // show: false
+    },
+    series: [{
+      name: 'A',
+      type: 'line',
+      smooth: true,
+      data: [18, 36, 65, 30, 78, 40, 33]
+    }]
   };
-  if (data && data.dimensions && data.measures) {
-    option.xAxis[0].data = data.dimensions.data
-    option.series = data.measures.map(item => {
-      return {
-        ...item,
-        type:'line',
-      }
-    })
-  }
+
   chart.setOption(option);
 }
 
@@ -49,14 +66,14 @@ export default class LineChart extends Component {
     }
   };
 
-  refresh(data) {
+  refresh() {
     this.Chart.init((canvas, width, height) => {
       const chart = echarts.init(canvas, null, {
         width: width,
         height: height
       });
 
-      setChartData(chart, data);
+      setChartData(chart);
       return chart;
     });
   }
--- src/pages/customer/customer_detail.tsx
+++ src/pages/customer/customer_detail.tsx
@@ -110,62 +110,7 @@ class CustomerDetail extends Component<Props, State> {
 
     this.pieChart.refresh(chartData);
 
-
-    const lineChartData = {
-      title: {
-        text: '测试下面legend的红色区域不应被裁剪',
-        left: 'center'
-      },
-      color: ["#37A2DA", "#67E0E3", "#9FE6B8"],
-      legend: {
-        data: ['A', 'B', 'C'],
-        top: 50,
-        left: 'center',
-        backgroundColor: 'red',
-        z: 100
-      },
-      grid: {
-        containLabel: true
-      },
-      tooltip: {
-        show: true,
-        trigger: 'axis'
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-        // show: false
-      },
-      yAxis: {
-        x: 'center',
-        type: 'value',
-        splitLine: {
-          lineStyle: {
-            type: 'dashed'
-          }
-        }
-        // show: false
-      },
-      series: [{
-        name: 'A',
-        type: 'line',
-        smooth: true,
-        data: [18, 36, 65, 30, 78, 40, 33]
-      }, {
-        name: 'B',
-        type: 'line',
-        smooth: true,
-        data: [12, 50, 51, 35, 70, 30, 20]
-      }, {
-        name: 'C',
-        type: 'line',
-        smooth: true,
-        data: [10, 30, 31, 50, 40, 20, 10]
-      }]
-    };
-
-    this.lineChart.refresh(lineChartData);
+    this.lineChart.refresh();
   }
 
 
