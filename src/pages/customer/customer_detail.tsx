e7ef76dacf26eb7ef2b5581998b65f5b9b69e3dc
波仔糕 <1054539528@qq.com>
2020-04-25 15:17:00 +0800

    fix

--- src/pages/customer/customer_detail.tsx
+++ src/pages/customer/customer_detail.tsx
@@ -79,9 +79,6 @@ class CustomerDetail extends Component<Props, State> {
    * 对于像 navigationBarTextStyle: 'black' 这样的推导出的类型是 string
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
-  config: Config = {
-    disableScroll: true
-  }
 
   constructor(props) {
     super(props);
